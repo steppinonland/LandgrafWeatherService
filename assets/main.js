c170be3f51b2306278a76e877a14649c51d1a6c8
Stephanie Landgraf <stlandgraf1234@gmail.com>
2020-05-21 22:26:50 -0500

    created the different elements for each of the 5 days

--- assets/main.js
+++ assets/main.js
@@ -1,9 +1,19 @@
 // This is our API key
-var APIKey = "appid=72dc3f69fdfc73eedbb7f9276f7e28db";
-var city = $("#city-search").val();
-var queryURL = "api.openweathermap.org/data/2.5/forecast?" + APIKey + city;
-// These are the default cities that show up in the list
 var cities = [];
+
+function displayCityWeather() {
+
+    var city = $(this).attr("data-name");
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&apikey=72dc3f69fdfc73eedbb7f9276f7e28db";
+
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).then(function(response) {
+      $("#cities-view").text(JSON.stringify(response));
+    });
+  }
+// These are the default cities that show up in the list
 function renderCities () {
 
     $("#my-cities-view").empty();
@@ -27,7 +37,7 @@ $("#add-city").on("click", function(event) {
     
     event.preventDefault();
     
-    var city = $(this).attr("data-name");
+    var city = $("#city-search").val().trim();
     
     cities.push(city);
     
@@ -38,22 +48,7 @@ $("#add-city").on("click", function(event) {
     renderCities();
 });
 // this will be the function that takes in the searched city and displays that data
-        function displayCityWeather() {
-            $.ajax({
-                url: queryURL,
-                method: "GET"
-            }).then(function(response) {               
-                $(".city").html("<h1>" + response.name + " Weather Details</h1>");
-                // var windSpeed = response.wind.speed;
-                // var cityTemp = (response.main.temp - 273.15) * 1.8 + 32;
-                // var UVindex = response.clouds.all;
-                // var humidity = response.main.humidity;
 
-
-            });
-
-        
-    }
 /* these are the ID's to target for just the weather elements:
 TODAY:
 "icon-today"
