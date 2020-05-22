14ac7298589954a1efa60db16a6ed45adddbf0e7
Stephanie Landgraf <stlandgraf1234@gmail.com>
2020-05-22 14:21:37 -0500

    got todays date and the city to show up

--- assets/main.js
+++ assets/main.js
@@ -1,52 +1,66 @@
-// This is our API key
+// Initial array of cities
 var cities = [];
 
+// Function for dumping the JSON content for each button into the div
 function displayCityWeather() {
+  $("#date").html(moment().format("dddd, MMMM Do YYYY"));
+  var city = $(this).attr("data-name");
+  var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&apikey=72dc3f69fdfc73eedbb7f9276f7e28db";
 
-    var city = $(this).attr("data-name");
-    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&apikey=72dc3f69fdfc73eedbb7f9276f7e28db";
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function(response) {
+    console.log(response);
+    $("#city-weather").html(JSON.stringify(response.name));
+  });
+}
 
-    $.ajax({
-      url: queryURL,
-      method: "GET"
-    }).then(function(response) {
-      $("#cities-view").text(JSON.stringify(response));
-    });
-  }
-// These are the default cities that show up in the list
-function renderCities () {
+// Function for displaying movie data
+function renderButtons() {
 
-    $("#my-cities-view").empty();
-// Looping through the array of cities
-    for (var i = 0; i < cities.length; i++) {
-  // Then dynamically generating a list for each movie in the array
-  // This code $("<li>") is all jQuery needs to create the beginning and end tag. (<li></li>)
-    var a = $("<li>");
-  // Adding a class to the list item
-    a.addClass("list-group-item");
-  // Adding a data-attribute
+  // Deleting the buttons prior to adding new movies
+  // (this is necessary otherwise you will have repeat buttons)
+  $("#buttons-view").empty();
+
+  // Looping through the array of movies
+  for (var i = 0; i < cities.length; i++) {
+
+    // Then dynamically generating buttons for each movie in the array
+    // This code $("<button>") is all jQuery needs to create the beginning and end tag. (<button></button>)
+    var a = $("<button>");
+    // Adding a class of movie to our button
+    a.addClass("city");
+    // Adding a data-attribute
     a.attr("data-name", cities[i]);
-  // Providing the initial list item text
+    // Providing the initial button text
     a.text(cities[i]);
-  // Adding the list item to the list-group div
-    $("#my-cities-view").append(a);
+    // Adding the button to the buttons-view div
+    $("#buttons-view").append(a);
+  }
 }
-}
-// this will be the function that takes in the searched city and displays that data
-$("#add-city").on("click", function(event) {
-    
-    event.preventDefault();
-    
-    var city = $("#city-search").val().trim();
-    
-    cities.push(city);
-    
-    console.log(cities);
-    
-    $(document).on("click", displayCityWeather);
 
-    renderCities();
+// This function handles events where one button is clicked
+$("#add-city").on("click", function(event) {
+  event.preventDefault();
+
+  // This line grabs the input from the textbox
+  var city = $("#city-input").val();
+
+  // Adding the movie from the textbox to our array
+  cities.push(city);
+  console.log(cities);
+
+  // Calling renderButtons which handles the processing of our movie array
+  renderButtons();
 });
+
+// Function for displaying the movie info
+// Using $(document).on instead of $(".movie").on to add event listeners to dynamically generated elements
+$(document).on("click", ".city", displayCityWeather);
+
+// Calling the renderButtons function to display the initial buttons
+renderButtons();
 // this will be the function that takes in the searched city and displays that data
 
 /* these are the ID's to target for just the weather elements:
